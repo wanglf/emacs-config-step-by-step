01f7ca6f9477220a14ec084dcd9336aba174f253
wanglf <wlfqware@gmail.com>
2017-04-11 13:30:57 +0800

    org-mode line warp

--- init.el
+++ init.el
@@ -25,6 +25,9 @@
 ;; powerline
 (require 'init-powerline)
 
+;; org-mode
+(require 'init-org)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ lisp/init-org.el
@@ -0,0 +1,5 @@
+;; line wrap
+(add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
+
+
+(provide 'init-org)
