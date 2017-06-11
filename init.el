8da483d2e52af3d5faee52a08a63e3dc05bbcda4
wanglf <wanglifeng.hz@gmail.com>
2017-06-11 10:30:51 +0800

    disable irc client and change ob-shell to ob-sh

--- init.el
+++ init.el
@@ -1,5 +1,12 @@
 ; suppress annoying messages about symbolic links
 ; to Git tracked files
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (setq vc-follow-symlinks nil)
 (setq vc-suppress-confirm t)
 
@@ -9,3 +16,5 @@
 (org-babel-load-file 
   (expand-file-name "~/.emacs.d/emacs-config.org"))
 
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
